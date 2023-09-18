60d6734ec13bc92d4e5429011d2a7fecd6dce893
suyanpengzhang <suyanpengzhang@gmail.com>
2023-09-18 15:04:11 -0700

    updates

--- model_speed.py
+++ model_speed.py
@@ -160,7 +160,7 @@ sol = []
 # =============================================================================
 #HL = np.ones(26)*np.array([random.random() for _ in range(26)])*totalpop
 HL = np.ones(26)*totalpop#*emp
-with open("Results/base_od_4time_emp_2lambda_odweighted.pkl", "rb") as file:
+with open("Results/base_od_4time_emp_1lambda_odweighted.pkl", "rb") as file:
     initials = pickle.load(file)
 dfxrand = initials[initials.name=='x']
 dfyrand = initials[initials.name=='y']
@@ -177,7 +177,7 @@ for limit_site in range(6,7):
         weights_bc = pickle.load(file)
     weights = 50*np.array(np.sum(value_weights,axis=1))/np.sum(np.array(value_weights))
     #weights = 50*np.array(weights_bc)/np.sum(np.array(weights_bc))
-    lambda_ = 2
+    lambda_ = 1
     try:
     
         # Create a new model
@@ -351,7 +351,7 @@ print(f"Runtime: {elapsed_time_hours:.2f} hours")
 # =============================================================================
 
 df = pd.DataFrame({'name': name, 'i': loc_i, 'j': loc_j, 'k': loc_k, 't': loc_t,'value':value_sol})
-df.to_pickle('Results/base_od_4time_total_2lambda_odweighted.pkl')
+df.to_pickle('Results/base_od_4time_total_1lambda_odweighted.pkl')
 print('saved')
 ################################
 #simple formulation
